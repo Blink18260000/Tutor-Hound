ad2956d2c9292f2a0c69a32e70a7a00e75f70f3d
Blink18260000 <esummins@gmail.com>
2016-03-03 18:08:14 -0800

    Footer content added

--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -5,7 +5,16 @@ var Footer = React.createClass({
   render: function () {
     return (
       <div className="footer-container">
-
+        <div className="footer-content">
+          <div className="footer-title">About the Author</div>
+          <div className="footer-link">Eric Summins</div>
+          <div className="footer-link">Email: esummins@gmail.com</div>
+          <div className="footer-link">Phone Number: (404)-313-0710</div>
+          <a className="footer-link"
+            href="https://github.com/Blink18260000" >Github Profile</a>
+          <a className="footer-link"
+            href="https://www.linkedin.com/in/esummins" >LinkedIn Profile</a>
+        </div>
       </div>
     );
   }
